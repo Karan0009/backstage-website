d5ef657961e68d500dfed93692bce072d7ed750f
karan singh <karansinghk0@gmail.com>
2021-02-12 16:14:32 +0530

    new changes

--- functions/registerform.js
+++ functions/registerform.js
@@ -1,32 +1,65 @@
+const { GoogleSpreadsheet } = require("google-spreadsheet")
+const joi = require("joi")
+
+const schema = joi.object({
+  name: joi.string().alphanum().min(3).max(100).required(),
+  instagram: joi.string().alphanum().max(200).required(),
+  tiktok: joi.string().alphanum().max(200).required(),
+  otherSocial: joi.string().max(200).empty(""),
+  country: joi.string().max(200).required(),
+  feedback: joi.string().alphanum().min(3).max(1000).required(),
+  phone: joi.number().required(),
+  email: joi.string().email().max(200).required(),
+})
+
 exports.handler = async function (event, context) {
   try {
-    const { GoogleSpreadsheet } = require("google-spreadsheet")
-    const doc = new GoogleSpreadsheet(process.env.spreadsheetId)
-    await doc.useServiceAccountAuth({
-      client_email: process.env.client_email,
-      private_key: process.env.private_key,
-    })
-    const info = await doc.loadInfo()
-
-    const sheet = doc.sheetsByIndex[0]
-    const newRow = await sheet.addRow({
-      name: "karan",
-      email: "karan@gmail.com",
-      feedback: "this is new",
-    })
-    console.log(process.env.testvar)
+    const body = JSON.parse(event.body)
+    const value = schema.validate({ ...body })
+    if (value.error) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "some error occured",
+          success: false,
+          error: value.error.details,
+        }),
+      }
+    } else {
+      const doc = new GoogleSpreadsheet(process.env.spreadsheetId)
+      await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY.replace(
+          /\\n/g,
+          "\n"
+        ),
+      })
+      await doc.loadInfo()
+      const sheet = doc.sheetsByIndex[0]
+      const newRow = await sheet.addRow({
+        name: body.name,
+        email: body.email,
+        feedback: body.feedback,
+        instagram: body.instagram,
+        tiktok: body.tiktok,
+        otherSocial: body.otherSocial,
+        phone: body.phone,
+        country: body.country,
+      })
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "data stored", success: true }),
+      }
+    }
   } catch (error) {
     console.log(error)
     return {
       statusCode: 400,
       body: JSON.stringify({
         message: "some error occured",
+        success: false,
         err: error.message,
       }),
     }
-  }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: process.env.testvar }),
   }
 }
--- src/components/registerFormSlide.js
+++ src/components/registerFormSlide.js
@@ -1,18 +1,203 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Input from "./input"
 import Textarea from "./textarea"
+import { required, emailValidator, number, length } from "../utils/validators"
 
 const RegisterFormSlide = props => {
+  const [username, setUsername] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, length({ min: 3, max: 100 })],
+    errorMessage: "Enter valid value",
+  })
+  const [instagram, setInstagram] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, length({ max: 200 })],
+    errorMessage: "Enter valid value",
+  })
+  const [tikTok, setTikTok] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, length({ max: 200 })],
+    errorMessage: "Enter valid value",
+  })
+  const [phoneNumber, setPhoneNumber] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, number],
+    errorMessage: "Enter valid value",
+  })
+  const [otherSocial, setOtherSocial] = useState({
+    value: "",
+    valid: true,
+    touched: false,
+    validators: [],
+    errorMessage: "",
+  })
+  const [email, setEmail] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, emailValidator],
+    errorMessage: "Enter valid value",
+  })
+  const [country, setCountry] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, length({ max: 200 })],
+    errorMessage: "Enter valid value",
+  })
+  const [feedback, setFeedback] = useState({
+    value: "",
+    valid: false,
+    touched: false,
+    validators: [required, length({ min: 3, max: 1000 })],
+    errorMessage: "Enter valid value",
+  })
+
+  const [formIsValid, setFormIsValid] = useState(false)
+
+  useEffect(() => {
+    if (
+      username.valid &&
+      instagram.valid &&
+      tikTok.valid &&
+      email.valid &&
+      country.valid &&
+      feedback.valid &&
+      phoneNumber.valid
+    )
+      setFormIsValid(true)
+    else setFormIsValid(false)
+  }, [username, instagram, tikTok, phoneNumber, email, country, feedback])
+
   const handleFormSubmit = async e => {
     try {
       e.preventDefault()
-      const response = await fetch("/api/registerform")
-      console.log(await response.json())
+      fetch("/.netlify/functions/registerform", {
+        method: "POST",
+        body: JSON.stringify({
+          name: username.value.trim(),
+          email: email.value.trim(),
+          phone: phoneNumber.value.trim(),
+          instagram: instagram.value.trim(),
+          tiktok: tikTok.value.trim(),
+          otherSocial: otherSocial.value.trim(),
+          country: country.value.trim(),
+          feedback: feedback.value.trim(),
+        }),
+      })
+        .then(response => {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
       props.openModal()
     } catch (error) {
       console.log(error)
     }
   }
+
+  //input change handlers
+  const usernameChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (username.validators)
+      for (const validator of username.validators) {
+        isValid = isValid && validator(value)
+      }
+    setUsername({ ...username, value: value, valid: isValid })
+  }
+  const emailChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (email.validators)
+      for (const validator of email.validators) {
+        isValid = isValid && validator(value)
+      }
+    setEmail({ ...email, value: value, valid: isValid })
+  }
+  const instagramChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (instagram.validators)
+      for (const validator of instagram.validators) {
+        isValid = isValid && validator(value)
+      }
+    setInstagram({ ...instagram, value: value, valid: isValid })
+  }
+  const tikTokChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (tikTok.validators)
+      for (const validator of tikTok.validators) {
+        isValid = isValid && validator(value)
+      }
+    setTikTok({ ...tikTok, value: value, valid: isValid })
+  }
+  const phoneNumberChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (phoneNumber.validators)
+      for (const validator of phoneNumber.validators) {
+        isValid = isValid && validator(value)
+      }
+    setPhoneNumber({
+      ...phoneNumber,
+      value: value,
+      valid: isValid,
+    })
+  }
+  const otherSocialChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (otherSocial.validators)
+      for (const validator of otherSocial.validators) {
+        isValid = isValid && validator(value)
+      }
+    setOtherSocial({
+      ...otherSocial,
+      value: value,
+      valid: isValid,
+    })
+  }
+  const countryChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (country.validators)
+      for (const validator of country.validators) {
+        isValid = isValid && validator(value)
+      }
+    setCountry({
+      ...country,
+      value: value,
+      valid: isValid,
+    })
+  }
+  const feedbackChangeHandler = e => {
+    const value = e.target.value
+    let isValid = true
+    if (feedback.validators)
+      for (const validator of feedback.validators) {
+        isValid = isValid && validator(value)
+      }
+    setFeedback({
+      ...feedback,
+      value: value,
+      valid: isValid,
+    })
+  }
+
   return (
     <>
       <div id="register" className="container register-form-slide">
@@ -21,7 +206,11 @@ const RegisterFormSlide = props => {
         </h1>
         <form
           className="register-form"
-          onSubmit={handleFormSubmit}
+          onSubmit={e => {
+            if (formIsValid) {
+              handleFormSubmit(e)
+            }
+          }}
           style={{
             margin: "80px 0",
           }}
@@ -30,64 +219,127 @@ const RegisterFormSlide = props => {
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="name"
               name="name"
               placeholder="Name"
               className="register-input left-input"
+              value={username.value}
+              valid={username.valid}
+              touched={username.touched}
+              errorMessage={username.errorMessage}
+              onChange={usernameChangeHandler}
+              onBlur={() => {
+                setUsername({ ...username, touched: true })
+              }}
             />
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="instagram"
               name="instagram"
               placeholder="Instagram"
               className="register-input"
+              value={instagram.value}
+              valid={instagram.valid}
+              touched={instagram.touched}
+              errorMessage={instagram.errorMessage}
+              onChange={instagramChangeHandler}
+              onBlur={() => {
+                setInstagram({ ...instagram, touched: true })
+              }}
             />
           </div>
           <div className="row input-row" style={{ justifyContent: "center" }}>
             <Input
               type="email"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="email"
               name="email"
               placeholder="Email"
               className="register-input left-input"
+              value={email.value}
+              valid={email.valid}
+              touched={email.touched}
+              errorMessage={email.errorMessage}
+              onChange={emailChangeHandler}
+              onBlur={() => {
+                setEmail({ ...email, touched: true })
+              }}
             />
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="tikTok"
               name="tikTok"
               placeholder="Tik Tok"
               className="register-input"
+              value={tikTok.value}
+              valid={tikTok.valid}
+              touched={tikTok.touched}
+              errorMessage={tikTok.errorMessage}
+              onChange={tikTokChangeHandler}
+              onBlur={() => {
+                setTikTok({ ...tikTok, touched: true })
+              }}
             />
           </div>
           <div className="row input-row" style={{ justifyContent: "center" }}>
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="phoneNumber"
               name="phoneNumber"
               placeholder="Phone"
               className="register-input left-input"
+              value={phoneNumber.value}
+              valid={phoneNumber.valid}
+              touched={phoneNumber.touched}
+              errorMessage={phoneNumber.errorMessage}
+              onChange={phoneNumberChangeHandler}
+              onBlur={() => {
+                setPhoneNumber({ ...phoneNumber, touched: true })
+              }}
             />
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="otherSocial"
               name="otherSocial"
               placeholder="other social"
               className="register-input"
+              value={otherSocial.value}
+              valid={otherSocial.valid}
+              touched={otherSocial.touched}
+              errorMessage={otherSocial.errorMessage}
+              onChange={otherSocialChangeHandler}
+              onBlur={() => {
+                setOtherSocial({ ...otherSocial, touched: true })
+              }}
             />
           </div>
           <div className="row input-row">
             <Input
               type="text"
               inputContainerClassName="register-input-container"
+              errorClassName="register-input-error-message"
               id="country"
               name="country"
               placeholder="Country"
               className="register-input"
+              value={country.value}
+              valid={country.valid}
+              touched={country.touched}
+              errorMessage={country.errorMessage}
+              onChange={countryChangeHandler}
+              onBlur={() => {
+                setCountry({ ...country, touched: true })
+              }}
             />
           </div>
           <div
@@ -102,15 +354,28 @@ const RegisterFormSlide = props => {
               placeholder="Feedback"
               inputContainerClassName="register-input-container register-textarea-container"
               className="register-input register-feedback"
+              errorClassName="register-input-error-message"
               style={{
                 height: "400px",
                 padding: "20px 30px",
                 width: "100%",
                 margin: "10px auto",
               }}
+              value={feedback.value}
+              valid={feedback.valid}
+              touched={feedback.touched}
+              errorMessage={feedback.errorMessage}
+              onChange={feedbackChangeHandler}
+              onBlur={() => {
+                setFeedback({ ...feedback, touched: true })
+              }}
             />
           </div>
-          <button className="register-btn" type="submit">
+          <button
+            className="register-btn"
+            type="submit"
+            disabled={formIsValid ? "" : "disabled"}
+          >
             Submit
           </button>
         </form>
